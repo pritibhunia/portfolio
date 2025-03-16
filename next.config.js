c028a8894da13a0a905979dcf296d3097d102005
Priti Bhunia <pritibhunia@gmail.com>
2025-03-16 18:19:34 +0100

    refactor next.config.js to simplify configuration and remove unnecessary properties

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
+// filepath: /Users/pritibhunia/repos/my-portfolio/next.config.js
 module.exports = {
-    output: 'export',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/pritibhunia.github.io/' : '',
-    basePath: process.env.NODE_ENV === 'production' ? '/pritibhunia.github.io' : '',
-    images: {
-      unoptimized: true,
-    },
-  };
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
